6c77b3b89b7f372aa8f82b9f9e0fb64f961f8c09
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-01 16:19:26 -0700

    add me query

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// createHttpLink
+
+// create authLink
+
+// create new ApolloClient
+
+// convert return to ApolloProvider
 function App() {
   return (
     <Router>
