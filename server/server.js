66f5b162787124ee462db88384dec22567d552a8
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-01 15:38:54 -0700

    finish adding apolloserver code

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
-const  { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const path = require('path');
+const  { ApolloServer } = require('apollo-server-express');
 
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
@@ -24,8 +27,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
